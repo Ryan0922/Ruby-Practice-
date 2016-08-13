5cf320089a1aea353bf8bd921b51a2e4cd7200a7
ryan0922 <hou.ryan@yahoo.com>
2016-08-13 15:25:33 -0400

    Gave user option to choose his or her desired operation

--- Non-Object-Oriented/analyzer.rb
+++ Non-Object-Oriented/analyzer.rb
@@ -27,8 +27,19 @@ input2 = gets.chomp
 abort("This is not a valid input") unless input2.to_i.to_s == input2
 number2 = input2.to_i
 
-puts "The addition of those numbers is equal to " + addNumbers(number1, number2).to_s
-puts "The difference of the two numbers is equal to " + subtractNumbers(number1, number2).to_s
-puts "The product of the two numbers is equal to " +multiplyNumbers(number1, number2).to_s
-puts "The quotient of the two numbers is equal to " +divideNumbers(number1, number2).to_s
-puts "The remainder of the division of the two numbers is equal to " +modulusNumbers(number1, number2).to_s
+puts "Select your desired operation:", "1. Addition", "2. Subtraction", "3. Multiplication", "4. Division", "5. Modulus"
+inputSelectOperation = gets.chomp.to_i
+case inputSelectOperation
+when 1
+  puts "The addition of those numbers is equal to " + addNumbers(number1, number2).to_s
+when 2
+  puts "The difference of the two numbers is equal to " + subtractNumbers(number1, number2).to_s
+when 3
+  puts "The product of the two numbers is equal to " +multiplyNumbers(number1, number2).to_s
+when 4
+  puts "The quotient of the two numbers is equal to " +divideNumbers(number1, number2).to_s
+when 5
+  puts "The remainder of the division of the two numbers is equal to " +modulusNumbers(number1, number2).to_s
+else
+  abort("This is not a valid input")
+end
