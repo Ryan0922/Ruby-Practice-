eac071cdcc7b533c5f9a74d918f6dab51fec4f58
ryan0922 <Ryan@Ryans-MacBook-Pro.local>
2016-08-13 14:48:29 -0400

    Created a subtraction function

--- Non-Object-Oriented/analyzer.rb
+++ Non-Object-Oriented/analyzer.rb
@@ -1,4 +1,15 @@
-def addNumbers (number1, number2)
-  number1+number2
+# def addNumbers (number1, number2)
+#   number1+number2
+# end
+# number1 = gets.chomp.to_i
+# number2 = gets.chomp.to_i
+#
+# puts "The addition of those numbers is = to " + addNumbers(number1, number2).to_s
+def subtractNumbers(number1, number2)
+  number1 - number2
 end
-puts addNumbers(3,4)
+puts "Type your first number"
+number1 = gets.chomp.to_i
+puts "Type your second number"
+number2 = gets.chomp.to_i
+puts "The difference of the two numbers is equal to " + subtractNumbers(number1, number2).to_s
